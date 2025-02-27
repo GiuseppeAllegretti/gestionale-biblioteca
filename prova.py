d53a3f9126c1e245af3c9fb9e11ce3f2bdfbf595
Giuseppe Allegretti <99804287+GiuseppeAllegretti@users.noreply.github.com>
2025-02-27 19:27:51 +0100

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,19 +1,1 @@
-class Studente:
-    oreSettimanali = 36
 
-    def __init__(self, nome, cognome, corsoStudi):
-        self.nome = nome
-        self.cognome = cognome
-        self.corsoStudi = corsoStudi
-    
-    def __str__(self):
-        return f"{self.nome} {self.cognome} {self.corsoStudi}\nore settimanali: {self.oreSettimanali}"
-
-
-studente1 = Studente ("giuseppe", "allegretti", "informatica")
-studente2= Studente ("marta", "stannis", "scienze")
-
-studente1.oreSettimanali +=4
-
-print (studente1)
-print (studente2)
